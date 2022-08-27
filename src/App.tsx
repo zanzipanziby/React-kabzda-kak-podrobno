858a691bda3cc36b4fb3f330f31cc4cbcba9b58a
ZanziPanziBY <zanzipanziby@gmail.com>
2022-08-28 01:24:36 +0300

    first pre-lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,50 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    debugger;
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <Rating />
+        <Accocdion />
     </div>
   );
+}
+
+
+function Rating(){
+    debugger;
+    return(
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>Star</div>
+    )
+}
+
+
+
+function Accocdion(){
+    debugger;
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Rating from './App';
+
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+
+
+
+
