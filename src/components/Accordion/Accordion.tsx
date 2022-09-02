7930537eb6af9a3d05c8491382f1828902ca2276
ZanziPanziBY <zanzipanziby@gmail.com>
2022-09-02 21:34:59 +0300

    срфchange Accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,28 +1,32 @@
 import React from "react";
+
 type AccordionPropsType = {
     titleValue: string;
     collapsed: boolean;
 }
+
 function Accordion(props: AccordionPropsType) {
     //debugger;
     if (props.collapsed) {
         return (
             <div>
                 <AccordionTitle title={props.titleValue}/>
-                <AccordionBody/>
             </div>
         )
-    } else  {
+    } else {
         return (
             <div>
                 <AccordionTitle title={props.titleValue}/>
+                <AccordionBody/>
             </div>
         )
     }
 }
+
 type AccordionTitlePropsType = {
     title: string;
 }
+
 function AccordionTitle(props: AccordionTitlePropsType) {
     return (
         <h3>{props.title}</h3>
